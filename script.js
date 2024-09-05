654fc8b57a03fcd713bd111ea20b4c00334cffcd
odi <101469962+0xip@users.noreply.github.com.>
2024-09-05 15:17:03 +0300

    Create getChange function

--- script.js
+++ script.js
@@ -37,4 +37,43 @@ let currencyUnits = [
     if(cashValue === price){
         change.innerText = "No change due - customer paid with exact cash";
     }
-  })    
+  });
+
+const getChange = (changeDue,cid) => {
+    let totalCid = parseFloat(cid.reduce((sum, [_, amount])=> sum + amount, 0).toFixed(2));
+    if(totalCid < changeDue){
+        return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+
+    let changeArr = [];
+    let remainingChange = changeDue;
+
+    for(let i = currencyUnits.length - 1; i>=0; i--){
+        let unit = currencyUnits[i][0];
+        let unitValue = currencyUnits[i][1];
+        let unitInDrawer = cid[i][1];
+
+        if(unitValue <= remainingChange && unitInDrawer > 0){
+            let amountFromUnit = 0;
+
+            while(remainingChange >= unitValue && unitInDrawer > 0){
+                remainingChange = (remainingChange - unitValue).toFixed(2);
+                unitInDrawer -=unitValue;
+                amountFromUnit += unitValue;
+            }
+            if(amountFromUnit > 0){
+                changeArr.push([unit, amountFromUnit])
+            }
+        }
+    }
+
+    if(remainingChange > 0){
+        return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+
+    if(changeDue === totalCid) {
+        return {status: "CLOSED", change: cid }
+    }
+
+    return {status: "OPEN", change: changeArr }
+} 
